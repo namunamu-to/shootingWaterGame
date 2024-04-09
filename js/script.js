b479392e68ffc85003a1cb30def24ae738cdb4ea
namunamu-to <ten1baniraSS@gmail.com>
2024-04-09 16:56:02 +0900

    左クリックと右クリックのイベントを追加

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 const ring = new Ring("gameDisplay", "100", "100");
 // ring.moveToGround();
 ring.move(100, 100);
+
+//水流出すハンドラ
+document.addEventListener("click", (e)=>{
+    console.log("aaa");
+
+});
+
+document.addEventListener("contextmenu", ()=>{
+    console.log("bbb");
+});
