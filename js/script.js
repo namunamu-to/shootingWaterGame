51c959adc44a327a2f3f8f7df65f7510435bdb06
namunamu-to <ten1baniraSS@gmail.com>
2024-04-09 17:21:29 +0900

    使いそうなイベントとハンドラをとりあえず定義

--- js/script.js
+++ js/script.js
@@ -2,12 +2,30 @@ const ring = new Ring("gameDisplay", "100", "100");
 // ring.moveToGround();
 ring.move(100, 100);
 
+//右クリック時にコンテキストメニューを表示させないようにする
+gameDisplay.addEventListener("contextmenu", ()=> {return false});
+
+//作成中
 //水流出すハンドラ
+function pushOutWaterFlow(, distance=100, weight="50"){
+    const tangent = (Math.max(fromX, toX) - Math.min(fromX, toX)) / (Math.max(fromY, toY) - Math.min(fromY, toY));
+    // const lineLength = 
+}
+
+//作成中
+//左クリックで左半分の画面側から水流出す
 document.addEventListener("click", (e)=>{
     console.log("aaa");
 
 });
 
+//作成中
+//右クリックで右半分の画面側から水流出す
 document.addEventListener("contextmenu", ()=>{
     console.log("bbb");
 });
+
+
+//ボールを落下させ続ける
+
+
