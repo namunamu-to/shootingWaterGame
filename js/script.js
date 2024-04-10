ac0f49fce0441c72ce3e38e62e1fa94f81884602
namunamu-to <ten1baniraSS@gmail.com>
2024-04-10 10:35:06 +0900

    右シフトキーも反応するようにした

--- js/script.js
+++ js/script.js
@@ -3,50 +3,49 @@ const ring = new Ring("gameDisplay", "100", "100");
 ring.move(100, 100);
 
 //右クリック時にコンテキストメニューを表示させないようにする
-gameDisplay.addEventListener("contextmenu", ()=> {return false});
+gameDisplay.addEventListener("contextmenu", () => { return false });
 
 //作成中
 //水流出すハンドラ
-function pushOutWaterFlow(distance=100, weight="50"){
+function pushOutWaterFlow(distance = 100, weight = "50") {
     // const lineLength = 
 }
 
 //作成中
 //左クリックで左半分のリングを右上方向に吹き飛ばす
-function blowRing(){
+function blowRing() {
     ring.moveTo(5, 5);
     console.log("aaa");
 }
-document.addEventListener("click", (e)=>{
+document.addEventListener("click", (e) => {
     // for(let i = 0; 0<40; i++) setTimeout(blowRing, 3);
-    
-
-});
-
-//左シフトキー
-document.addEventListener("keydown", (e) => {
-    if (e.code === "ShiftLeft") {
-        console.log("AAA");
-    }
 });
 
 //作成中
 //右クリックで右半分の画面側から水流出す
-document.addEventListener("contextmenu", ()=>{
+document.addEventListener("contextmenu", () => {
     console.log("bbb");
 });
 
-//右シフトキー
-//自分のキーボードだと右シフトキーを判別してくれない？
-//自分の右シフトキー自体は羽後kので入手していない？
+//左シフトキー
 document.addEventListener("keydown", (e) => {
-    if (e.code === "ShiftRight") {
+    if (e.code === "ShiftLeft") { //左シフト
+        console.log("AAA");
+    }
+    if (e.location == 0) { //右シフト
         console.log("BBB");
     }
+
+    console.log("e.code = " + e.code);
+    console.log("e.location = " + e.location);
+    console.log("KeyboardEvent.DOM_KEY_LOCATION_LEFT = " + KeyboardEvent.DOM_KEY_LOCATION_LEFT);
+    console.log("KeyboardEvent.DOM_KEY_LOCATION_RIGHT = " + KeyboardEvent.DOM_KEY_LOCATION_RIGHT);
+
 });
 
+
 //ボールを落下させ続ける
-function fallRing(){
+function fallRing() {
     ring.move(0, 3);
 }
 setInterval(fallRing, 11);
