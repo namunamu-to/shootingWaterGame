7b8f9ef66c16a83f300e9f0e01c99f66dcd50ddb
namunamu-to <ten1baniraSS@gmail.com>
2024-04-12 16:17:33 +0900

    リファクタリングした

--- js/Ring.js
+++ js/Ring.js
@@ -2,6 +2,7 @@ const showElm = document.getElementById("gameDisplay"); //表示先の要素
 let rings = [];
 const ringImgPaths = { "red": "./img/redRing.png", "blue": "./img/blueRing.png" }
 let createdNum = 0;
+const ringSize = 40;
 
 function createRing(x = 0, y = 0, color = "red") {
     id = ++createdNum;
@@ -11,6 +12,8 @@ function createRing(x = 0, y = 0, color = "red") {
     elm.setAttribute("class", "ring");
     elm.setAttribute("id", id);
     elm.setAttribute("src", ringImgPaths[color]);
+    elm.style.width = ringSize + "px";
+    elm.style.height = ringSize + "px";
     showElm.appendChild(elm);
 
     //引数で指定された座標に描画
@@ -33,18 +36,44 @@ function createRing(x = 0, y = 0, color = "red") {
     return elm;
 }
 
+//int型の要素のx座標を取得する関数
+function getX(elm){
+    return parseInt(elm.style.left);
+}
+
+function setX(elm, x){
+    elm.style.left = x + "px";
+}
+
+function setY(elm, y){
+    elm.style.top = y + "px";
+}
+
+//int型の要素のy座標を取得する関数
+function getY(elm){
+    return parseInt(elm.style.top);
+}
+
+//int型の要素の横幅を取得する関数
+function getWidth(elm){
+    return parseInt(elm.clientWidth);
+}
+
+//int型の要素の縦幅を取得する関数
+function getHeight(elm){
+    return parseInt(elm.clientHeight);
+}
+
 //どこの側面にいるか
 function whichSide(elm) {
-    const leftInt = parseInt(elm.style.left);
-    const showElmWidth = parseInt(showElm.clientWidth);
-    const topInt = parseInt(elm.style.top);
-    const showElmHeight = parseInt(showElm.clientHeight);
+    const elmX = getX(elm);
+    const elmY = getY(elm);
 
     let nowSide = { "top": false, "bottom": false, "right": false, "left": false };
-    nowSide["left"] = leftInt < 0; //左端
-    nowSide["right"] = leftInt > showElmWidth - parseInt(elm.clientWidth); //右端
-    nowSide["top"] = topInt < 0; //上端
-    nowSide["bottom"] = topInt > showElmHeight - parseInt(elm.clientHeight); //下端
+    nowSide["left"] = elmX < 0; //左端
+    nowSide["right"] = elmX > getWidth(showElm) - getWidth(elm); //右端
+    nowSide["top"] = elmY < 0; //上端
+    nowSide["bottom"] = elmY > getHeight(showElm) - getWidth(elm); //下端
 
     return nowSide;
 }
@@ -52,13 +81,11 @@ function whichSide(elm) {
 //ゲーム画面からはみ出していたら、はみ出さないようにする
 function restoreFromMoveOut(elm) {
     const nowSide = whichSide(elm);
-    const showElmWidth = parseInt(showElm.clientWidth);
-    const showElmHeight = parseInt(showElm.clientHeight);
 
-    if (nowSide["left"]) elm.style.left = "0px"; //左端
-    else if (nowSide["right"]) elm.style.left = (showElmWidth - parseInt(elm.clientWidth)) + "px"; //右端
-    else if (nowSide["top"]) elm.style.top = "0px"; //上端
-    else if (nowSide["bottom"]) elm.style.top = (showElmHeight - parseInt(elm.clientHeight)) + "px"; // 下端
+    if (nowSide["left"]) setX(elm, 0); //左端
+    else if (nowSide["right"]) setX(elm, getWidth(showElm) - getWidth(elm)); //右端
+    else if (nowSide["top"]) setY(elm, 0); //上端
+    else if (nowSide["bottom"]) setY(elm, getHeight(showElm) - getHeight(elm)); // 下端
 
     elm.style.zIndex = 1000 + id; //この行がないとringが表示されなくなる。
 }
@@ -68,12 +95,10 @@ function restoreFromMoveOut(elm) {
 function move(elm, x = 0, y = 0) {
     restoreFromMoveOut(elm);
 
-    //
-
     //移動
     elm.style.position = "relative";
-    elm.style.left = parseInt(elm.style.left) + x + "px";
-    elm.style.top = parseInt(elm.style.top) + y + "px";
+    setX(elm, getX(elm) + x);
+    setY(elm, getY(elm) + y);
     elm.style.position = "absolute";
 
     restoreFromMoveOut(elm);
@@ -91,16 +116,14 @@ function repeatMove(elm, oneMoveAmmountX, oneMoveAmmountY, interval, numberOfMov
 
 
 //リングの生成
-const ringWidth = 40;
-const ringHeight = 40;
 for (let i = 0; i < 8; i++) {
-    const randomX = parseInt(Math.random() * (parseInt(gameDisplay.clientWidth) - ringWidth)); //0～ (gameDIsplay - ringHeight)の横幅の範囲でランダム
-    const romdomY = parseInt(Math.random() * (parseInt(gameDisplay.clientHeight) - ringHeight)); //0～　(gameDIsplay縦幅 - ringHeight)の範囲でランダム
+    const randomX = parseInt(Math.random() * (getWidth(gameDisplay) - ringSize)); //0～ (gameDIsplay - ringSize)の横幅の範囲でランダム
+    const romdomY = parseInt(Math.random() * (getHeight(gameDisplay) - ringSize)); //0～　(gameDIsplay縦幅 - ringSize)の範囲でランダム
     rings.push(createRing(randomX, romdomY, color = "blue"));
 }
 
 for (let i = 0; i < 8; i++) {
-    const randomX = parseInt(Math.random() * (parseInt(gameDisplay.clientWidth) - ringWidth)); //0～ (gameDIsplay - ringHeight)の横幅の範囲でランダム
-    const romdomY = parseInt(Math.random() * (parseInt(gameDisplay.clientHeight) - ringHeight)); //0～　(gameDIsplay縦幅 - ringHeight)の範囲でランダム
+    const randomX = parseInt(Math.random() * (getWidth(gameDisplay) - ringSize)); //0～ (gameDIsplay - ringSize)の横幅の範囲でランダム
+    const romdomY = parseInt(Math.random() * (getHeight(gameDisplay) - ringSize)); //0～　(gameDIsplay縦幅 - ringSize)の範囲でランダム
     rings.push(createRing(randomX, romdomY, color = "red"));
 }
